bbf41d19a11116c70598361aeaf102ebea00cdd1
michaelenco <michael.enco@gmail.com>
2018-02-16 17:41:37 +0300

    more native-like wkwebview

--- trusty/ViewController.swift
+++ trusty/ViewController.swift
@@ -19,11 +19,35 @@ class ViewController: UIViewController {
     
     override func loadView() {
         super.loadView()
-        let webConfiguration = WKWebViewConfiguration()
-        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        
+        //scale config
+        let viewportScriptString = "var meta = document.createElement('meta'); meta.setAttribute('name', 'viewport'); meta.setAttribute('content', 'width=device-width'); meta.setAttribute('initial-scale', '1.0'); meta.setAttribute('maximum-scale', '1.0'); meta.setAttribute('minimum-scale', '1.0'); meta.setAttribute('user-scalable', 'no'); document.getElementsByTagName('head')[0].appendChild(meta);"
+        
+        //disable selection and callouts
+        let disableSelectionScriptString = "document.documentElement.style.webkitUserSelect='none';"
+        let disableCalloutScriptString = "document.documentElement.style.webkitTouchCallout='none';"
+        
+        let viewportScript = WKUserScript(source: viewportScriptString, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        let disableSelectionScript = WKUserScript(source: disableSelectionScriptString, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        let disableCalloutScript = WKUserScript(source: disableCalloutScriptString, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        
+        
+        let controller = WKUserContentController()
+        
+        controller.addUserScript(viewportScript)
+        controller.addUserScript(disableSelectionScript)
+        controller.addUserScript(disableCalloutScript)
+        
+        let config = WKWebViewConfiguration()
+        config.userContentController = controller
+        webView = WKWebView(frame: .zero, configuration: config)
+        
+        webView.scrollView.bounces = false
+        webView.allowsBackForwardNavigationGestures = false
+        webView.contentMode = .scaleToFill
 
         webView.uiDelegate = self
-
+        webView.navigationDelegate = self
         
         view.addSubview(webView)
         webView.translatesAutoresizingMaskIntoConstraints = false
@@ -49,5 +73,9 @@ class ViewController: UIViewController {
 extension ViewController: WKUIDelegate {
     
 }
-
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("nav finish");
+    }
+}
 
