3364b964640bc590b0476d5c364418deb158b63d
michaelenco <michael.enco@gmail.com>
2018-03-21 00:46:56 +0300

    javascrip interface for fcm token retrieving

--- trusty/AppDelegate.swift
+++ trusty/AppDelegate.swift
@@ -7,23 +7,143 @@
 //
 
 import UIKit
+import UserNotifications
+
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    let gcmMessageIDKey = "gcm.message_id"
+    
+    func registerForNotifications() {
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: { _, _ in })
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            UIApplication.shared.registerUserNotificationSettings(settings)
+        }
+        
+        UIApplication.shared.registerForRemoteNotifications()
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        /////////////
+        FirebaseApp.configure()
+        
+        Messaging.messaging().delegate = self
+        
+        //////////////
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.backgroundColor = .black
         window?.makeKeyAndVisible()
         
         window?.rootViewController = ViewController()
-        
+        setStatusBarBackgroundColor(color: UIColor(colorLiteralRed: 30/255, green: 34/255, blue: 37/255, alpha: 1))
+
         return true
     }
+    func setStatusBarBackgroundColor(color: UIColor) {
+        
+        guard let statusBar = UIApplication.shared.value(forKeyPath: "statusBarWindow.statusBar") as? UIView else { return }
+        
+        statusBar.backgroundColor = color
+    }
     
+    // [START receive_message]
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        // If you are receiving a notification message while your app is in the background,
+        // this callback will not be fired till the user taps on the notification launching the application.
+        // TODO: Handle data of notification
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        // Messaging.messaging().appDidReceiveMessage(userInfo)
+        // Print message ID.
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID3: \(messageID)")
+        }
+        
+        // Print full message.
+        print(userInfo)
+    }
+
+    // [END receive_message]
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Unable to register for remote notifications: \(error.localizedDescription)")
+    }
+    
+    // This function is added here only for debugging purposes, and can be removed if swizzling is enabled.
+    // If swizzling is disabled then this function must be implemented so that the APNs token can be paired to
+    // the FCM registration token.
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("APNs token retrieved: \(deviceToken)")
+        
+        // With swizzling disabled you must set the APNs token here.
+        //Messaging.messaging().apnsToken = deviceToken
+        
+    }
+}
+
+// [START ios_10_message_handling]
+@available(iOS 10, *)
+extension AppDelegate : UNUserNotificationCenterDelegate {
+    
+    // Receive displayed notifications for iOS 10 devices.
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        // Messaging.messaging().appDidReceiveMessage(userInfo)
+        // Print message ID.
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID1: \(messageID)")
+        }
+        
+        // Print full message.
+        print(userInfo)
+        
+        // Change this to your preferred presentation option
+        completionHandler([])
+    }
+}
+// [END ios_10_message_handling]
+
+extension AppDelegate : MessagingDelegate {
+    // [START refresh_token]
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+        print("Firebase receive registration registration token: \(fcmToken)")
+        NotificationCenter.default.post(
+            name: Notification.Name("newToken"),
+            object: fcmToken)
+    }
+    // [END refresh_token]
+    // [START refresh_token]
+    func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
+        print("Firebase refresh registration token: \(fcmToken)")
+        NotificationCenter.default.post(
+            name: Notification.Name("newToken"),
+            object: fcmToken)
+        
+        // TODO: If necessary send token to application server.
+        // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+    // [END refresh_token]
+    // [START ios_10_data_message]
+    // Receive data messages on iOS 10+ directly from FCM (bypassing APNs) when the app is in the foreground.
+    // To enable direct data messages, you can set Messaging.messaging().shouldEstablishDirectChannel to true.
+    func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) {
+        print("Received data message: \(remoteMessage.appData)")
+    }
+    // [END ios_10_data_message]
 }
 
--- trusty/ViewController.swift
+++ trusty/ViewController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 import WebKit
+import UserNotifications
+import Firebase
+import JavaScriptCore
 
 class ViewController: UIViewController {
     
@@ -17,29 +20,57 @@ class ViewController: UIViewController {
         return .lightContent
     }
     
+    func newToken(notification: NSNotification) {
+        setToken(token: (notification.object as? String) ?? "")
+    }
+    func setToken(token: String) {
+        webView.evaluateJavaScript("if(_GlobalIOSCallback) _GlobalIOSCallback('\(token)');",
+            completionHandler: { _, _ in })
+    }
+    
     override func loadView() {
         super.loadView()
         
-        //scale config
-        let viewportScriptString = "var meta = document.createElement('meta'); meta.setAttribute('name', 'viewport'); meta.setAttribute('content', 'width=device-width'); meta.setAttribute('initial-scale', '1.0'); meta.setAttribute('maximum-scale', '1.0'); meta.setAttribute('minimum-scale', '1.0'); meta.setAttribute('user-scalable', 'no'); document.getElementsByTagName('head')[0].appendChild(meta);"
-        
-        //disable selection and callouts
-        let disableSelectionScriptString = "document.documentElement.style.webkitUserSelect='none';"
-        let disableCalloutScriptString = "document.documentElement.style.webkitTouchCallout='none';"
-        
-        let viewportScript = WKUserScript(source: viewportScriptString, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
-        let disableSelectionScript = WKUserScript(source: disableSelectionScriptString, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
-        let disableCalloutScript = WKUserScript(source: disableCalloutScriptString, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
-        
-        
         let controller = WKUserContentController()
         
+        //scale config
+        let viewportScriptString = "var meta = document.createElement('meta'); meta.setAttribute('name', 'viewport'); meta.setAttribute('content', 'width=device-width'); meta.setAttribute('initial-scale', '1.0'); meta.setAttribute('maximum-scale', '1.0'); meta.setAttribute('minimum-scale', '1.0'); meta.setAttribute('user-scalable', 'no'); document.getElementsByTagName('head')[0].appendChild(meta);"
+        let viewportScript = WKUserScript(source: viewportScriptString, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
         controller.addUserScript(viewportScript)
+        
+        //disable selection
+        let disableSelectionScriptString = "document.documentElement.style.webkitUserSelect='none';"
+        let disableSelectionScript = WKUserScript(source: disableSelectionScriptString, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
         controller.addUserScript(disableSelectionScript)
+        //disable callouts
+        
+        let disableCalloutScriptString = "document.documentElement.style.webkitTouchCallout='none';"
+        let disableCalloutScript = WKUserScript(source: disableCalloutScriptString, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
         controller.addUserScript(disableCalloutScript)
+        
+        let redirectConsoleScriptString = "document.addEventListener('message', function(e){ window.webkit.messageHandlers.iosListener.postMessage(e.data); })"
+        let redirectConsoleScript = WKUserScript(source: redirectConsoleScriptString, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        controller.addUserScript(redirectConsoleScript)
+        
+        let subscribeFunctionScriptString = "function subscribeFCMToken(callback) { " +
+        "_GlobalIOSCallback = callback; " +
+        "window.webkit.messageHandlers.iosListener.postMessage('subscribeFCM'); " +
+        "};"
+        let subscribeFunctionScript = WKUserScript(source: subscribeFunctionScriptString, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        controller.addUserScript(subscribeFunctionScript)
+        
+        /*let testFNScriptString = "function testFn() { return 'testString'; }"
+        let testFNScript = WKUserScript(source: testFNScriptString, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        controller.addUserScript(testFNScript)*/
+        
+        
+        let preferences = WKPreferences()
+        preferences.javaScriptEnabled = true
         
         let config = WKWebViewConfiguration()
         config.userContentController = controller
+        config.userContentController.add(self, name: "iosListener")
+        config.preferences = preferences
         webView = WKWebView(frame: .zero, configuration: config)
         
         webView.scrollView.bounces = false
@@ -57,11 +88,18 @@ class ViewController: UIViewController {
             webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             webView.trailingAnchor.constraint(equalTo: view.trailingAnchor)])
         //webView.backgroundColor = .black
+
+        NotificationCenter.default.addObserver(self, selector: #selector(newToken), name: NSNotification.Name("newToken"), object: nil)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let myURL = URL(string: "http://beta.trusty.fund")
+        WKWebsiteDataStore.default().removeData(
+            ofTypes: [WKWebsiteDataTypeDiskCache, WKWebsiteDataTypeMemoryCache],
+            modifiedSince: Date(timeIntervalSince1970: 0),
+            completionHandler: {});
+        
+        let myURL = URL(string: "http://192.168.10.36:8080/index.html")
         let myRequest = URLRequest(url: myURL!)
         webView.load(myRequest)
         setNeedsStatusBarAppearanceUpdate()
@@ -80,9 +118,31 @@ extension ViewController: WKNavigationDelegate {
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         print("nav finish")
     }
+    /*func printHTML() {
+        webView.evaluateJavaScript("document.documentElement.outerHTML.toString()",
+                                   completionHandler: { (html: Any?, error: Error?) in
+                                    print(html)
+        })
+    }*/
     func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
         print("failed")
         print(error)
     }
 }
 
+extension ViewController: WKScriptMessageHandler{
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage)
+    {
+        switch (message.body as! String) {
+        case "subscribeFCM":
+            if let token = Messaging.messaging().fcmToken {
+                setToken(token: token)
+            }
+            (UIApplication.shared.delegate as! AppDelegate).registerForNotifications()
+            
+            break
+        default:
+            print("message: \(message.body)")
+        }
+    }
+}
